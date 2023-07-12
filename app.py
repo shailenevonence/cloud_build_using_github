db25b1b8ba072d7ce4084665bef04de537409a62
shailenevonence <shailendra@evonence.com>
2023-07-12 16:35:07 +0530

    code updated

--- app.py
+++ app.py
@@ -1,19 +1,10 @@
 from flask import Flask
-import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    branch_name = os.getenv('BRANCH_NAME', 'unknown')
-    if branch_name == 'evonence':
-        return 'Hello World ! Evonence'
-    elif branch_name == 'master':
-        return 'Hello World ! master'
-    elif branch_name == 'ncorium':
-        return 'Hello World ! Ncorium'
-    else:
-        return 'Hello World ! Unknown Branch'
+    return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=5000)
